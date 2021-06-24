6afa6002a91a07029c146b085d84788b3497ec22
Antoine Chalifour <antoine.chalifour@octo.com>
2021-06-24 10:50:08 +0200

    navbar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,19 +1,59 @@
+import Link from "next/link";
 import { useUser } from "@auth0/nextjs-auth0";
+
+import { PokeBall } from "@/components/PokeBall";
+
+const UserProfile = ({ user }) => (
+  <div>
+    <p>Welcome {user.given_name}</p>
+    {/* eslint-disable-next-line @next/next/no-img-element */}
+    <img src={user.picture} />
+    {/* eslint-disable-next-line @next/next/no-html-link-for-pages */}
+    <a href="/api/auth/logout">Logout</a>
+
+    <style jsx>{`
+      div {
+        display: grid;
+        grid-template-columns: auto 30px auto;
+        align-items: center;
+        grid-gap: 2rem;
+      }
+
+      img {
+        display: block;
+        width: 100%;
+        border-radius: 50%;
+      }
+    `}</style>
+  </div>
+);
 
 export const NavBar = () => {
   const { user } = useUser();
 
   return (
     <nav>
+      <Link href="/">
+        <a aria-label="Go back to home page">
+          <PokeBall />
+        </a>
+      </Link>
+
+      {/* eslint-disable-next-line @next/next/no-html-link-for-pages */}
       {!user && <a href="/api/auth/login">Login</a>}
-      {user && <a href="/api/auth/logout">Logout</a>}
+      {user && <UserProfile user={user} />}
 
       <style jsx>{`
         nav {
           position: sticky;
           top: 0;
           z-index: 10;
+
+          display: flex;
+          align-items: center;
+          justify-content: space-between;
           padding: 2rem;
+
           background: rgba(0, 0, 0, 0.75);
           backdrop-filter: blur(5px);
         }
---
+++ src/components/PokeBall.js
@@ -0,0 +1,56 @@
+export const PokeBall = () => (
+  <span>
+    <style jsx>{`
+      span {
+        position: relative;
+
+        display: block;
+        width: 3rem;
+        aspect-ratio: 1;
+        border: 2px solid #111;
+        border-radius: 50%;
+        overflow: hidden;
+
+        background: #d90707;
+
+        cursor: pointer;
+        transform: scale(1);
+        transition: transform 0.25s ease;
+      }
+
+      span:hover {
+        transform: scale(1.1);
+      }
+
+      span:before {
+        content: "";
+        position: absolute;
+        bottom: 0;
+
+        display: block;
+        height: 50%;
+        width: 100%;
+        border-top: 2px solid #111;
+
+        background: #fff;
+      }
+
+      span:after {
+        content: "";
+        position: absolute;
+        top: 50%;
+        left: 50%;
+
+        display: block;
+        width: 0.8rem;
+        aspect-ratio: 1;
+        border: 2px solid #111;
+        border-radius: 50%;
+
+        background: #fff;
+
+        transform: translate(-50%, -50%);
+      }
+    `}</style>
+  </span>
+);
