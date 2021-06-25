4293542254038b2677029c2533aed96705d558cb
Antoine Chalifour <antoine.chalifour@octo.com>
2021-06-25 09:11:44 +0200

    fix paginated api results

--- src/server/CardsHTTPAdapter.js
+++ src/server/CardsHTTPAdapter.js
@@ -1,4 +1,30 @@
-export const CardsHTTPAdapter = (http) => ({
-  ofSet: (setId) =>
-    http.get(`/cards?q=set.id:${setId}`).then((res) => res.data.data),
-});
+export const CardsHTTPAdapter = (http) => {
+  const CardsOfSetFetcher = (setId) => (page) =>
+    http.get(`/cards?q=set.id:${setId}&page=${page}`).then((res) => res.data);
+
+  async function* pagesOfSet(setId) {
+    const fetchPage = CardsOfSetFetcher(setId);
+    let page = 1;
+
+    while (true) {
+      const { data, pageSize, count } = await fetchPage(page);
+
+      yield data;
+
+      if (count < pageSize) return;
+      else page += 1;
+    }
+  }
+
+  return {
+    ofSet: async (setId) => {
+      const cards = [];
+
+      for await (const nextCards of pagesOfSet(setId)) {
+        cards.push(...nextCards);
+      }
+
+      return cards;
+    },
+  };
+};
