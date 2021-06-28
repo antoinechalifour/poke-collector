1d196dc366cc133ea07291ea4d43cbfa7cba1aa1
Antoine Chalifour <antoine.chalifour@octo.com>
2021-06-28 19:31:53 +0200

    add card count on home page

--- src/components/HomePage/CollectionProgress.js
+++ src/components/HomePage/CollectionProgress.js
@@ -1,6 +1,31 @@
 import { useUser } from "@auth0/nextjs-auth0";
 import useSWR from "swr";
 
+const ProgressBar = ({ progress }) => (
+  <div>
+    <span style={{ width: `${progress}%` }} />
+
+    <style jsx>{`
+      div {
+        position: absolute;
+        bottom: 0;
+        left: 0;
+        right: 0;
+
+        height: 0.3rem;
+        background: var(--color-background-lighter);
+      }
+
+      span {
+        position: absolute;
+        height: 100%;
+        left: 0;
+        background: var(--color-accent);
+      }
+    `}</style>
+  </div>
+);
+
 export const CollectionProgress = ({ setId }) => {
   const { user } = useUser();
   const { data } = useSWR(user ? "/api/me/collection/progress" : null);
@@ -9,25 +34,26 @@ export const CollectionProgress = ({ setId }) => {
   const { progress, collected, total } = data[setId];
 
   return (
-    <div className="root">
-      <div style={{ width: `${progress}%` }} />
+    <div>
+      <p>
+        {collected}/{total}
+      </p>
+
+      <ProgressBar progress={progress} />
 
       <style jsx>{`
-        .root {
+        div {
           position: absolute;
           bottom: 0;
           left: 0;
           right: 0;
-
-          height: 0.3rem;
-          background: var(--color-background-lighter);
         }
 
-        .root div {
-          position: absolute;
-          height: 100%;
-          left: 0;
-          background: var(--color-accent);
+        p {
+          text-align: right;
+          padding: 1rem;
+          font-weight: bold;
+          font-size: var(--size-text-md);
         }
       `}</style>
     </div>
