b6fcfd92669ed01472114bd34345809cc3022c0b
Antoine Chalifour <antoine.chalifour@octo.com>
2021-07-09 13:45:39 +0200

    remove pwa

--- next.config.js
+++ next.config.js
@@ -1,17 +1,10 @@
-const withPWA = require("next-pwa");
 const withBundleAnalyzer = require("@next/bundle-analyzer")({
   enabled: process.env.ANALYZE === "true",
 });
 
-module.exports = withPWA(
-  withBundleAnalyzer({
-    reactStrictMode: true,
-    images: {
-      domains: ["images.pokemontcg.io", "assets.pokemon.com"],
-    },
-    pwa: {
-      disable: process.env.NODE_ENV === "development",
-      dest: "public",
-    },
-  })
-);
+module.exports = withBundleAnalyzer({
+  reactStrictMode: true,
+  images: {
+    domains: ["images.pokemontcg.io", "assets.pokemon.com"],
+  },
+});
--- pages/_app.js
+++ pages/_app.js
@@ -5,37 +5,9 @@ import "@algolia/autocomplete-theme-classic";
 import "../styles/globals.css";
 
 import { UserProvider } from "@auth0/nextjs-auth0";
-import { useEffect } from "react";
-
-function useServiceWorkerUpdates() {
-  useEffect(() => {
-    if (
-      typeof window !== "undefined" &&
-      "serviceWorker" in navigator &&
-      window.workbox !== undefined
-    ) {
-      const wb = window.workbox;
-      const promptNewVersionAvailable = () => {
-        if (
-          confirm(
-            "A new version of Poké Collector has been released, reload to update?"
-          )
-        ) {
-          wb.messageSkipWaiting();
-
-          window.location.reload();
-        }
-      };
-
-      wb.addEventListener("waiting", promptNewVersionAvailable);
-    }
-  }, []);
-}
 
 function MyApp({ Component, pageProps }) {
   const getLayout = Component.getLayout || ((page) => page);
-
-  useServiceWorkerUpdates();
 
   return (
     <UserProvider>
